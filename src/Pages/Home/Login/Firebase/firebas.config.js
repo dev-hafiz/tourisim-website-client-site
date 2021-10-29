a241fb0ce2fcc4444759e4d415221ef7366c0472
okshi <johnstarhafiz@gmail.com>
2021-10-29 14:29:18 +0600

    This is the env file setup commit

--- src/Pages/Home/Login/Firebase/firebas.config.js
+++ src/Pages/Home/Login/Firebase/firebas.config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-     apiKey: "AIzaSyAmS7j7Xza7JUXPzISdsdbz5oHHSUajYCw",
-     authDomain: "travel-agency-authentication.firebaseapp.com",
-     projectId: "travel-agency-authentication",
-     storageBucket: "travel-agency-authentication.appspot.com",
-     messagingSenderId: "312458160206",
-     appId: "1:312458160206:web:860e3bfdc0651d2c517b1f"
+     apiKey: process.env.REACT_APP_API_KEY,
+     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+     projectId: process.env.REACT_APP_PROJECT_ID,
+     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+     appId: process.env.REACT_APP_APP_ID,
    };
 
 export default firebaseConfig;
+
